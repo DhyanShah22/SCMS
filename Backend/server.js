57e23a5d6c286672e3f0f749922c2fa58b3120df
DhyanShah22 <dhyanshah229@gmail.com>
2024-03-27 19:47:46 +0530

    DBMS 13: Server file updated..

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,16 @@ const helmet = require('helmet')
 const morgan = require('morgan');
 const cors = require('cors')
 require('dotenv').config()
-const routes = require('./Routes/routes')
 const bodyParser = require('body-parser');
 const { syncModels } = require('./Models/models');
+
+
+const supplierRoutes = require('./Routes/supplierRoutes')
+const customerRoutes = require('./Routes/customerRoutes')
+const inventoryRoutes = require('./Routes/inventoryRoutes')
+const orderItemsRoutes = require('./Routes/orderItemsRoutes')
+const orderRoutes = require('./Routes/orderRoutes')
+const productRoutes = require('./Routes/productRoutes')
 
 const app = express()
 app.use(morgan('dev'));
@@ -16,7 +23,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors()); 
 
-app.use('/api', routes)
+app.use('/api', supplierRoutes)
+app.use('/api', productRoutes)
+app.use('/api', inventoryRoutes)
+app.use('/api', customerRoutes)
+app.use('/api', orderRoutes)
+app.use('/api', orderItemsRoutes)
+
 
 
 app.use((req, res, next) => {
